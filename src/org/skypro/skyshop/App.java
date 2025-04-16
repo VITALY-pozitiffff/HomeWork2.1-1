4df4fe594130101dd7a0ef2159d8b83a21da2c08
white <White_pozitiv@mail.ru>
2025-04-16 21:43:18 +0300

    DZ continuation furher

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -1,18 +1,37 @@
 package org.skypro.skyshop;
 
 import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.content.Article;
 import org.skypro.skyshop.product.DiscountedProduct;
 import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.product.SimpleProduct;
+import org.skypro.skyshop.search.SearchEngine;
+
+import java.util.Arrays;
 
 
 public class App {
     public static void main(String[] args) {
-        ProductBasket bucket = new ProductBasket();
-        bucket.addProduct(new SimpleProduct("Обычный товар", 500));
-        bucket.addProduct(new DiscountedProduct("Товар со скидкой", 1000, 20));
-        bucket.addProduct(new FixPriceProduct("Товар FIX PRICE"));
-        bucket.printContents();
+        SearchEngine searchEngine = new SearchEngine(10);
+
+
+        searchEngine.add(new SimpleProduct("Обычный товар", 500));
+        searchEngine.add(new DiscountedProduct("Товар со скидкой", 1000, 20));
+        searchEngine.add(new FixPriceProduct("Товар fix price"));
+
+
+        searchEngine.add(new Article("Обзор смартфона", "Отличный телефон с большим экраном"));
+        searchEngine.add(new Article("Как выбрать ноутбук", "Советы по выбору ноутбука"));
+
+
+        System.out.println("Поиск по слову 'товар':");
+        System.out.println(Arrays.toString(searchEngine.search("товар")));
+
+        System.out.println("\nПоиск по слову 'обзор':");
+        System.out.println(Arrays.toString(searchEngine.search("обзор")));
+
+        System.out.println("\nПоиск по слову 'телефон':");
+        System.out.println(Arrays.toString(searchEngine.search("телефон")));
     }
 }
--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -1,27 +1,48 @@
 package org.skypro.skyshop.product;
 
-public abstract class Product {
-    private String name;
+import org.skypro.skyshop.search.Searchable;
 
+public abstract class Product implements Searchable {
+    private  String name;
 
     public Product(String name) {
         this.name = name;
     }
 
+    @Override
+    public String getSearchTerm() {
+        return getName();
+    }
+
+    @Override
+    public String getContentType() {
+        return "PRODUCT";
+    }
+
+    @Override
     public String getName() {
         return name;
     }
+
+
 
     public abstract double getPrice();
 
     public boolean isSpecial() {
         return false;
 
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return getName() + ": " + getPrice();
+    }
+
+
 }
 
-@Override
-public String toString() {
-    return getName() + ": " + getPrice();
-}
-}
 
